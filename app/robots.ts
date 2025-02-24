9fc563ed2f87531c49cf53f5172899a21a43eca3
Riandre van der Voorden <20230503@ctucareer.co.za>
2025-02-24 14:13:01 +0200

    Removed old robots.txt file

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,18 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://riandre.com'
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/private/',
+          '/admin/',
+        ],
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   }
 }
